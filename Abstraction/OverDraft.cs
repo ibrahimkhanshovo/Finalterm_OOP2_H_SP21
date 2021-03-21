024a501e5aeaa512a5810ec8550b321145504293
Ibrahim Khan Shovo <78006923+ibrahimkhanshovo@users.noreply.github.com>
2021-03-21 14:55:48 +0600

    Abstraction

---
+++ Abstraction/OverDraft.cs
@@ -0,0 +1,68 @@
+﻿            using System;
+            using System.Collections.Generic;
+            using System.Linq;
+            using System.Text;
+            using System.Threading.Tasks;
+
+namespace Abstraction
+    {
+        class OverDraft : Account
+        {
+            public int Limit { get; set; }
+            public OverDraft()
+            {
+
+            }
+            public OverDraft(string accName, string accNo, double balance, int limit) : base(accName, accNo, balance)
+            {
+                Limit = limit;
+            }
+
+            public override void Deposit(double amount)
+            {
+                Balance = Balance + Limit + amount;
+                Console.WriteLine("Your acc is credited by {0} to self and updated  balance is {1} ", amount, Balance);
+            }
+            public override void Withdraw(double amount)
+            {
+                if (amount <= (Balance + Limit))
+                {
+                    Balance = Balance - amount;
+                    Console.WriteLine("Your acc is debited by {0} and updated  balance is {1} ", amount, Balance);
+                }
+                else
+                {
+
+                    Console.WriteLine("Insuffient Balance maximum amount you can withdraw is:" + (Balance + Limit));
+                }
+            }
+            public override void Transfer(Account a, double amount)
+            {
+                if (amount <= (Balance + Limit))
+                {
+                    Balance = Balance - amount;
+
+
+                    Console.WriteLine("You account is debited by {0} new balance {1} and credited to  acc: {2} ({3})", amount, Balance, a.AccName, a.AccNo);
+                }
+                else
+                {
+                    Console.WriteLine("Insuffient Balance maximum amount you can transfer is:" + (Balance + Limit));
+                }
+
+            }
+
+            public override void ShowInfo()
+            {
+                Console.WriteLine("Name: " + AccName);
+                Console.WriteLine("Acc No: " + AccNo);
+
+            }
+
+
+        }
+
+    }
+}
+    }
+}
---
+++ Abstraction/program.cs1
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account A = new SpecialSaving("Bishowjit", "000111222", 20, 10);
+            Account B = new SpecialSaving("Rahim", "000111223", 300.50, 200.50);
+
+            Account C = new OverDraft("Karim", "1122334", 20, 5);
+            Account D = new OverDraft("Jon", "22233344", 8000.75, 5000);
+
+            A.ShowInfo();
+            A.Deposit(5);
+            A.Withdraw(20);
+            A.Transfer(B, 2);
+
+            Console.WriteLine("*************************************");
+
+            C.ShowInfo();
+            C.Deposit(15);
+            C.Withdraw(15);
+            C.Transfer(D, 10);
+            
+
+
+
+
+        }
+    }
+}
